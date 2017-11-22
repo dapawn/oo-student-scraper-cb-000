37d44a887abb1cec050e5cc5d8fa051319e0ec1b
David Kuruvilla <dapawn@gmail.com>
2017-11-22 16:42:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attribute_hash.each {|key, value| self.send(("#{key}="), value)}
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.all
